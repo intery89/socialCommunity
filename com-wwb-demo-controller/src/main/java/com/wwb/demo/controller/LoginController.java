04ec1ca7f33732b2671f1a5e8e31b5b2329a80b6
xiai87@qq.com <xiai87@qq.com>
2016-05-15 22:05:28 +0800

    返回结果的改写

--- com-wwb-demo-controller/src/main/java/com/wwb/demo/controller/LoginController.java
+++ com-wwb-demo-controller/src/main/java/com/wwb/demo/controller/LoginController.java
@@ -3,6 +3,8 @@ package com.wwb.demo.controller;
 import com.wwb.demo.domain.model.LoginForm;
 import com.wwb.demo.service.impl.LoginService;
 import com.wwb.demo.utils.ValidationCodeGenerator;
+import com.wwb.demo.utils.result.ResultResponse;
+import com.wwb.demo.utils.result.enums.ResultResponseCode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -23,22 +25,27 @@ public class LoginController {
     @Autowired
     LoginService loginService;
 
-    @RequestMapping(value = "/login",method = RequestMethod.POST)
-    public ModelAndView login(@ModelAttribute("LoginUser") LoginForm user, HttpSession session){
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ModelAndView login(@ModelAttribute("LoginUser") LoginForm user, HttpSession session) {
         String username = user.getUsername();
         String password = user.getPassword();
-        boolean isClient = loginService.checkUserIdentification(username, password);
 
-        if(isClient == true) {
+        ResultResponse resultResponse = loginService.checkUserIdentification(username, password);
+
+        if (resultResponse.getStatus().getCode().equals(ResultResponseCode.SUCCESS.getCode())) {
+            //需要将member的相关数据放入到session中
             return new ModelAndView("base/index.jsp");
-        }else{
+        } else if (resultResponse.getStatus().getCode().equals(ResultResponseCode.USERNAME_PASSWORD_ERROR.getCode())) {
             return new ModelAndView("base/login.jsp");
+        } else {
+            //抛出异常或者是其他错误
+            return new ModelAndView("base/error.jsp");
         }
     }
 
     @ResponseBody
     @RequestMapping(value = "/generateValidateCode")
-    public String generateValidationCode(HttpSession session){
+    public String generateValidationCode(HttpSession session) {
         try {
             List<String> codeAndPath = ValidationCodeGenerator.generateCode(pic);
             session.setAttribute("code", codeAndPath.get(1));
@@ -53,14 +60,14 @@ public class LoginController {
 
     @ResponseBody
     @RequestMapping(value = "/checkValidateCode", method = RequestMethod.GET)
-    public boolean generateValidationCode(@PathVariable("inputCode")String inputCode, HttpSession session){
+    public boolean generateValidationCode(@PathVariable("inputCode") String inputCode, HttpSession session) {
         try {
             String code = (String) session.getAttribute("code");
-            if(code.equalsIgnoreCase(inputCode)){
+            if (code.equalsIgnoreCase(inputCode)) {
                 return true;
             }
             return false;
-        }catch(Exception e){
+        } catch (Exception e) {
             return false;
         }
     }
