fd88b5cdb2d40d5f19f025043f21a56b24eb6c91
xiai87@qq.com <xiai87@qq.com>
2016-05-15 22:05:01 +0800

    返回结果的改写

--- com-wwb-demo-service/src/main/java/com/wwb/demo/service/impl/LoginService.java
+++ com-wwb-demo-service/src/main/java/com/wwb/demo/service/impl/LoginService.java
@@ -1,7 +1,10 @@
 package com.wwb.demo.service.impl;
 
+import com.wwb.demo.dao.MemberDao;
 import com.wwb.demo.domain.model.Member;
 import com.wwb.demo.utils.CipherUtils;
+import com.wwb.demo.utils.result.ResultResponse;
+import com.wwb.demo.utils.result.enums.ResultResponseCode;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -12,19 +15,25 @@ public class LoginService {
     @Autowired
     MemberDao memberDao;
 
-    public String decipher(String password){
+    public String decipher(String password) {
         return CipherUtils.convertMD5(CipherUtils.convertMD5(password));
     }
 
-    public boolean checkUserIdentification(String username, String password){
-        Member member = memberDao.find(username);
-        if(member != null) {
-            String pwd = member.getPassword();
-            if (pwd.equals(decipher(password))) {
-                return true;
+    public ResultResponse checkUserIdentification(String username, String password) {
+        ResultResponse resultResponse = null;
+        try {
+            Member member = memberDao.getMemberByUserName(username);
+            if (member != null) {
+                String pwd = member.getPassword();
+                if (pwd.equals(decipher(password))) {
+                    resultResponse = new ResultResponse(ResultResponseCode.SUCCESS, true);
+                }
+            } else {
+                resultResponse = new ResultResponse(ResultResponseCode.USERNAME_PASSWORD_ERROR, false);
             }
+        } catch (Exception e) {
+            resultResponse = new ResultResponse(ResultResponseCode.USERNAME_PASSWORD_ERROR, false);
         }
-        return false;
+        return resultResponse;
     }
-
 }
