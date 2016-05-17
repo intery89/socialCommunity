8ac59cffaab117637771c3155c7359678a76540e
intery <inspiration123@163.com>
2016-05-17 23:47:17 +0800

    modify controller to accept and return json data

--- com-wwb-demo-controller/src/main/java/com/wwb/demo/controller/LoginController.java
+++ com-wwb-demo-controller/src/main/java/com/wwb/demo/controller/LoginController.java
@@ -12,7 +12,9 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Intery on 2016/5/15.
@@ -26,7 +28,8 @@ public class LoginController {
     LoginService loginService;
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public ModelAndView login(@ModelAttribute("LoginUser") LoginForm user, HttpSession session) {
+    @ResponseBody
+    public ResultResponse login(@RequestBody LoginForm user, HttpSession session) {
         String username = user.getUsername();
         String password = user.getPassword();
 
@@ -34,41 +37,48 @@ public class LoginController {
 
         if (resultResponse.getStatus().getCode().equals(ResultResponseCode.SUCCESS.getCode())) {
             //需要将member的相关数据放入到session中
-            return new ModelAndView("base/index.jsp");
+            return resultResponse;
         } else if (resultResponse.getStatus().getCode().equals(ResultResponseCode.USERNAME_PASSWORD_ERROR.getCode())) {
-            return new ModelAndView("base/login.jsp");
+            return resultResponse;
         } else {
             //抛出异常或者是其他错误
-            return new ModelAndView("base/error.jsp");
+            return resultResponse;
         }
     }
 
     @ResponseBody
     @RequestMapping(value = "/generateValidateCode")
-    public String generateValidationCode(HttpSession session) {
+    public Map<String, String> generateValidationCode(HttpSession session) {
+        Map<String, String> result = new HashMap<String, String>();
         try {
             List<String> codeAndPath = ValidationCodeGenerator.generateCode(pic);
             session.setAttribute("code", codeAndPath.get(1));
-            return codeAndPath.get(2);
+            result.put("path", codeAndPath.get(2));
+            return result;
         } catch (IOException e) {
             e.printStackTrace();
             session.setAttribute("code", "");
             String path = pic + "default.jpg";
-            return path;
+            result.put("path", path);
+            return result;
         }
     }
 
+    @RequestMapping(value = "/checkValidateCode", method = RequestMethod.POST)
     @ResponseBody
-    @RequestMapping(value = "/checkValidateCode", method = RequestMethod.GET)
-    public boolean generateValidationCode(@PathVariable("inputCode") String inputCode, HttpSession session) {
+    public Map<String, String> generateValidationCode(@RequestBody Map<String, String> inputCode, HttpSession session) {
+        Map<String, String> result = new HashMap<String, String>();
         try {
             String code = (String) session.getAttribute("code");
-            if (code.equalsIgnoreCase(inputCode)) {
-                return true;
+            if (code.equalsIgnoreCase(inputCode.get("code"))) {
+                result.put("result", "success");
+                return result;
             }
-            return false;
+            result.put("result", "false");
+            return result;
         } catch (Exception e) {
-            return false;
+            result.put("result", "false");
+            return result;
         }
     }
 }
